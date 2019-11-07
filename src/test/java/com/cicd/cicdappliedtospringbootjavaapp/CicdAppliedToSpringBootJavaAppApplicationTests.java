5ea776b6267f1420b34eeabe71cf50cd21230560
sunilchandra007 <sunilchandra007@gmail.com>
2019-11-08 00:45:22 +0800

    Update CicdAppliedToSpringBootJavaAppApplicationTests.java

--- src/test/java/com/cicd/cicdappliedtospringbootjavaapp/CicdAppliedToSpringBootJavaAppApplicationTests.java
+++ src/test/java/com/cicd/cicdappliedtospringbootjavaapp/CicdAppliedToSpringBootJavaAppApplicationTests.java
@@ -1,13 +1,22 @@
 package com.cicd.cicdappliedtospringbootjavaapp;
 
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
 
 @SpringBootTest
 class CicdAppliedToSpringBootJavaAppApplicationTests {
 
+	@Autowired
+	private CicdAppliedToSpringBootJavaAppApplication helloController;
+
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
+		
+		Assertions.assertEquals("Hello world from dzone",helloController.sayHello() );
 	}
 
 }
